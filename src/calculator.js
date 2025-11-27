a544e7bae87992e9232d91da74008f9051580165
Camille <Camille>
2025-11-27 18:39:23 +0100

    delete useless functions

--- src/calculator.js
+++ src/calculator.js
@@ -121,21 +121,6 @@ function operate()
     }
 }
 
-function add(a, b)
-{
-    return a + b;
-}
-
-function subtract(a, b)
-{
-    return a - b;
-}
-
-function multiply(a, b)
-{
-    return a * b;
-}
-
 function divide(a, b)
 {
     if(b == 0) {
