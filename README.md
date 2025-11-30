06b1a092afc68572e0501ad7f9c85b86ed6590a5
Camille <Camille>
2025-11-30 19:57:34 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my solution to the [calculator app](https://www.theodinproject.com/lessons/foundations-calculator) from The Odin Project.
+
+It rounds numbers to 5 digits to avoid floating point number errors such as $0.1 + 0.2 \neq 0.3$.
+
+![](screenshot.png)
---
+++ src/calculator.js
@@ -0,0 +1,139 @@
+class Calculator
+{
+ constructor(display, result, comma) {
+    this.display = display;
+    this.result = result;
+    this.comma = comma;
+
+    this.left = "";
+    this.right = "";
+    this.operator = "";
+
+    this.operations = {
+        "+": (a, b) => a + b,
+        "−": (a, b) => a - b,
+        "×": (a, b) => a * b,
+        "÷": (a, b) => this.divide(a, b)
+    };
+}
+
+// Calculator logic
+
+getDigit(d)
+{
+    (this.operator === "") ? this.left += d : this.right += d;
+    this.displayOperation();
+}
+
+getComma() {
+    if(this.operator === "") {
+        this.left = this.makeValid(this.left) + '.';
+    } else {
+        this.right = this.makeValid(this.right) + '.';
+    }
+    this.disableComma();
+    this.displayOperation();
+}
+
+getOperator(str)
+{
+    this.left = this.makeValid(this.left);
+    if(this.operator != "") this.operate();
+    this.operator = str;
+    this.enableComma();
+    this.displayOperation();
+}
+
+operate()
+{
+    if(this.operator !== "") this.right = this.makeValid(this.right);
+    this.displayOperation();
+    this.display.textContent += " =";
+
+    this.left = this.operations[this.operator](Number(this.left), Number(this.right));
+    if(Number.isNaN(this.left)) {
+        this.reset();
+    }
+    this.left = Math.round((Number(this.left) + Number.EPSILON) * 100000) / 100000;
+    this.left = (this.left === 0) ? "" : this.left = this.left.toString();
+    this.right = "";
+    this.operator = "";
+
+    this.displayResult();
+    this.left.includes('.') ? this.disableComma() : this.enableComma();
+}
+
+// UI-related functions
+
+displayOperation()
+{
+    this.display.textContent = `${this.left} ${this.operator} ${this.right}`;
+}
+
+displayResult()
+{
+    this.result.textContent = Number(this.left);
+}
+
+disableComma()
+{
+    this.comma.disabled = true;
+}
+
+enableComma()
+{
+    this.comma.disabled = false;
+}
+
+// Control functions
+
+reset()
+{
+    this.left = "";
+    this.right = "";
+    this.operator = "";
+    this.enableComma();
+    this.displayOperation();
+    this.displayResult();
+}
+
+del()
+{
+    if(this.right != "") {
+        if(this.right.endsWith('.')) this.enableComma();
+        this.right = this.right.slice(0, -1);
+    } else if(this.operator != "") {
+        this.operator = "";
+    } else {
+        if(this.left.endsWith('.')) this.enableComma();
+        this.left = this.left.slice(0, -1);
+        if(this.left === "") this.displayResult();
+    }
+
+    this.displayOperation();
+}
+
+// Auxilary functions
+
+makeValid(str) {
+    if(str === "") return "0";
+    if(str.endsWith('.')) return str.slice(0, -1);
+    return str;
+}
+
+divide(a, b)
+{
+    if(b === 0) {
+        alert("Math Error");
+        return NaN;
+    }
+    return a / b;
+}
+
+} //end Calculator
+
+const comma = document.getElementById("comma");
+const display = document.getElementById("display");
+const result = document.getElementById("result");
+
+const calculator = new Calculator(display, result, comma);
